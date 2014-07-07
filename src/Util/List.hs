c5ebcf866d7018941b39736791365e21b54d3456
andrea falconi <andrea.falconi@gmail.com>
2014-07-07 21:21:43 +0200

    added List utils---NB unused at the moment, might remove.

---
+++ src/Util/List.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE UnicodeSyntax #-}
+module Util.List
+    ( projectIndexes
+    )
+where
+
+import Prelude.Unicode
+import Data.List
+
+
+
+--
+-- projects elements of input list at specified indexes.
+-- input list is indexed starting from 0 on (i.e. x₀, x₁, etc.)
+-- e.g.
+--      projectIndexes [] []           ⇝ []
+--      projectIndexes [1] []          ⇝ []
+--      projectIndexes [] [0]          ⇝ []
+--      projectIndexes [0] [0]         ⇝ [0]
+--      projectIndexes [] [0..9]       ⇝ []
+--      projectIndexes [1] [0..9]      ⇝ [1]
+--      projectIndexes [1,0] [0..9]    ⇝ [0,1]
+--      projectIndexes [1,0,-1] [0..9] ⇝ [0,1]
+--      projectIndexes [1..6] [0..9]   ⇝ [1,2,3,4,5,6]
+--
+projectIndexes ∷ [Integer] → [α] → [α]
+projectIndexes ixs xs = project (sort ixs) (zip [0..] xs)
+    where
+    project ixs@(i:is) zs@((j,x):rest)
+            | i < j = project is zs
+            | i ≡ j = x : project is rest
+            | i > j = project ixs rest
+    project _ _ = []
